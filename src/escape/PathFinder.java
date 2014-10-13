dfab7e04c977be0f0effed88028be9881fcf5987
superpingu <arnaudbonetti@me.com>
2014-10-14 00:11:50 +0200

    initial commit - contains path finding algorythme test code : displays the path in blue

---
+++ src/escape/PathFinder.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escape;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author arnaud
+ */
+public class PathFinder {
+    private EscapeGrid grid;
+    
+    public PathFinder(EscapeGrid grid) {
+        this.grid = grid;
+    }
+    public void shorterPath(EscapeBlock root, EscapeBlock target) {
+        ArrayList<EscapeBlock> waitingBlocks = new ArrayList<>();
+        EscapeBlock current;
+        grid.clearMarks();
+        
+        root.setMarked(true);
+        waitingBlocks.add(root);
+        
+        while(waitingBlocks.size()>0) {
+            current = waitingBlocks.get(0);
+            waitingBlocks.remove(0);
+            for(EscapeBlock block : current.getNeighbours()) {
+                if(!block.isMarked()) {
+                    block.setMarked(true);
+                    block.setFather(current);
+                    if(block.equals(target)) {
+                        paintItBlue(root, target); // TEST
+                        return;
+                    }
+                    waitingBlocks.add(block);
+                }
+            }
+        }
+    }
+    public void shorterPath(int[] rootLocation, int[] targetLocation) {
+        shortedPath(rootLocation[0],rootLocation[1],targetLocation[0],targetLocation[1]);
+    }
+    public void shortedPath(int rootx, int rooty, int targetx, int targety) {
+        shorterPath(grid.getBlock(rootx, rooty), grid.getBlock(targetx, targety));
+    }
+    public void paintItBlue(EscapeBlock root,EscapeBlock target) {
+        EscapeBlock current = target;
+        while(!current.equals(root)) {
+            current.setAliveColor(new Color(56,177,239));
+            current = current.getFather();
+        }
+        root.setAliveColor(new Color(56,177,239));
+    }
+}
